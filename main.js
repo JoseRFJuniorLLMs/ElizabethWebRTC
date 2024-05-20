3487964b3594d56ba3bd7bf9b4cb64a3f6a84831
Jose RF Junior <web2ajax@gmail.com>
2024-05-20 06:06:39 +0100

    ate que fim

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const servers = {
   iceCandidatePoolSize: 10,
 };
 
-const pc = new RTCPeerConnection(servers);
+let pc = null;
 let localStream = null;
 let remoteStream = null;
 
@@ -35,6 +35,20 @@ const webcamVideo = document.getElementById('webcamVideo');
 const remoteVideo = document.getElementById('remoteVideo');
 const finishCallButton = document.getElementById('finishCallButton');
 
+const localControls = document.getElementById('localControls');
+const remoteControls = document.getElementById('remoteControls');
+
+const initializePC = () => {
+  pc = new RTCPeerConnection(servers);
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+};
+
+initializePC();
+
 startButton.onclick = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
   remoteStream = new MediaStream();
@@ -42,12 +56,6 @@ startButton.onclick = async () => {
   localStream.getTracks().forEach((track) => {
     pc.addTrack(track, localStream);
   });
-
-  pc.ontrack = (event) => {
-    event.streams[0].getTracks().forEach((track) => {
-      remoteStream.addTrack(track);
-    });
-  };
 
   webcamVideo.srcObject = localStream;
   remoteVideo.srcObject = remoteStream;
@@ -143,6 +151,94 @@ finishCallButton.onclick = async () => {
   localStream.getTracks().forEach(track => track.stop());
   remoteStream.getTracks().forEach(track => track.stop());
 
+  // Reinitialize the RTCPeerConnection for a new call
+  initializePC();
   finishCallButton.disabled = true;
   startButton.disabled = false;
 };
+
+const addVideoControlListeners = (controlsContainer, videoElement) => {
+  controlsContainer.querySelectorAll('input[type="range"], select').forEach(control => {
+    control.addEventListener('input', (event) => {
+      const { id, value } = event.target;
+      switch (id) {
+        case 'localBrightness':
+        case 'remoteBrightness':
+          videoElement.style.filter = `brightness(${value})`;
+          break;
+        case 'localContrast':
+        case 'remoteContrast':
+          videoElement.style.filter += ` contrast(${value})`;
+          break;
+        case 'localSaturation':
+        case 'remoteSaturation':
+          videoElement.style.filter += ` saturate(${value})`;
+          break;
+        case 'localSepia':
+        case 'remoteSepia':
+          videoElement.style.filter += ` sepia(${value})`;
+          break;
+        case 'localGrayscale':
+        case 'remoteGrayscale':
+          videoElement.style.filter += ` grayscale(${value})`;
+          break;
+        case 'localInvert':
+        case 'remoteInvert':
+          videoElement.style.filter += ` invert(${value})`;
+          break;
+        case 'localGamma':
+        case 'remoteGamma':
+          videoElement.style.filter += ` contrast(${value})`;
+          break;
+        case 'localVolume':
+        case 'remoteVolume':
+          videoElement.volume = value;
+          break;
+        case 'localPan':
+        case 'remotePan':
+          videoElement.style.objectPosition = `${value * 100}% center`;
+          break;
+        case 'localTilt':
+        case 'remoteTilt':
+          videoElement.style.transform = `rotate(${value}deg)`;
+          break;
+        case 'localZoom':
+        case 'remoteZoom':
+          videoElement.style.transform += ` scale(${value})`;
+          break;
+        case 'localResolution':
+        case 'remoteResolution':
+          changeResolution(videoElement, value);
+          break;
+      }
+    });
+  });
+};
+
+const changeResolution = (videoElement, resolution) => {
+  switch (resolution) {
+    case 'qvga':
+      videoElement.width = 320;
+      videoElement.height = 240;
+      break;
+    case 'vga':
+      videoElement.width = 640;
+      videoElement.height = 480;
+      break;
+    case 'hd':
+      videoElement.width = 1280;
+      videoElement.height = 720;
+      break;
+    case 'fullhd':
+      videoElement.width = 1920;
+      videoElement.height = 1080;
+      break;
+    default:
+      videoElement.width = 640;
+      videoElement.height = 480;
+      break;
+  }
+};
+
+addVideoControlListeners(localControls, webcamVideo);
+addVideoControlListeners(remoteControls, remoteVideo);
